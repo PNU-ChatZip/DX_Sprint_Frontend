15d11c469d04e5e6822983717db7e02d8ee5052a
daehee <gkseogml4959@gmail.com>
2023-07-18 02:05:16 +0900

    api 연결 및 영상 재생바 관련 코드 작성

--- public/background.js
+++ public/background.js
@@ -2,6 +2,7 @@ console.log("[Youtube Summary Extension] connected...");
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension 설치 완료");
+  console.log("github 연결");
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
--- public/contentscript.js
+++ public/contentscript.js
@@ -54,7 +54,39 @@ async function insertSummary() {
     const container = document.getElementById("yt-script-container");
     // container.innerText = transcript;
 
-    const sections = await getSections();
+    const data = await (
+      await fetch("https://waterboom.iptime.org:1032/?videoId=2RU1_CXCPbw")
+    ).json();
+
+    // const data = [
+    //   {
+    //     time: "00:21",
+    //     text: "자막에서 데드풀이라는 캐릭터를 언급하며 캐릭터의 디자인과 표현을 칭찬합니다",
+    //   },
+    //   {
+    //     time: "02:47",
+    //     text: "화자는 흥분을 표현하며 프로페셔널의 완벽한 작품이라고 칭찬합니다",
+    //   },
+    //   {
+    //     time: "04:46",
+    //     text: "화자는 세 번째 줄에서 모든 걱정이 사라지고 공연의 완벽함을 칭찬하며 보여준 실력에 놀라움과 감탄을 표현하고 있습니다",
+    //   },
+    //   {
+    //     time: "07:19",
+    //     text: "이 영상은 미술계에서 흔히 볼 수 없는 고미나와 이세돌의 팬아트를 등장시켜 독특하고 신선한 컨셉을 선보이고 있습니다",
+    //   },
+    //   {
+    //     time: "09:30",
+    //     text: "화자는 곧 출시될 작품에 대한 기대감을 표현하며, 가는 곳마다 불을 마주치는 코난과 비슷한 캐릭터가 등장할 것임을 암시합니다",
+    //   },
+    //   {
+    //     time: "12:37",
+    //     text: "말레피센트는 해커 대회에서 우승하며 뛰어난 실력을 칭찬받습니다",
+    //   },
+    // ];
+
+    const sections = await getSections(data);
+    chrome.runtime.sendMessage(sections);
     sections.map((section) => {
       container.appendChild(section);
       const hr = document.createElement("hr");
@@ -62,10 +94,43 @@ async function insertSummary() {
     });
 
     // 전체 영상을 %단위로 쪼갠 데이터
-    const playerBarInfo = [10, 20, 10, 30, 10, 20];
+    // const playerBarInfo = [10, 20, 10, 30, 10, 20];
+    // 예제 영상 길이 = 13:18
+    // https://www.youtube.com/watch?v=tvI18HF3aaM
+    const playerBarInfo = getPlayerBarInfo(
+      data,
+      document.querySelector("video").duration
+    );
     await insertPlayerBar(playerBarInfo);
   }
 }
+
+// totalVideoPlaytime : second(초)
+function getPlayerBarInfo(data, totalVideoPlaytime) {
+  const playerBarInfo = [];
+  let preInfo = 0;
+  for (let i = 0; i < data.length; i++) {
+    const timeData = data[i].time.split(":").map((t) => parseInt(t));
+    nowInfo = ((timeData[0] * 60 + timeData[1]) / totalVideoPlaytime) * 100;
+    playerBarInfo.push(nowInfo - preInfo);
+    preInfo = nowInfo;
+  }
+  playerBarInfo.push(100 - preInfo);
+  chrome.runtime.sendMessage(playerBarInfo);
+  return playerBarInfo;
+}
+
+// async function makePlayerBar(playerBarInfo) {
+//   const bars = [];
+//   playerBarInfo.map((info) => {
+//     const bar = document.createElement("div");
+//     bar.className =
+//       "ytp-chapter-hover-container ytp-exp-chapter-hover-container ytp-chapter-bar";
+//     bar.style.width = `${info}%`;
+//     bars.push(bar);
+//   });
+//   return bars;
+// }
 
 async function makePlayerBar(playerBarInfo) {
   const bars = [];
@@ -74,6 +139,34 @@ async function makePlayerBar(playerBarInfo) {
     bar.className =
       "ytp-chapter-hover-container ytp-exp-chapter-hover-container ytp-chapter-bar";
     bar.style.width = `${info}%`;
+
+    const barPadding = document.createElement("div");
+    barPadding.className = "ytp-progress-bar-padding";
+    bar.appendChild(barPadding);
+    const ytpProgressBar = document.createElement("div");
+    ytpProgressBar.className = "ytp-progress-list";
+    const progressElementInfo = [
+      {
+        className: "ytp-play-progress ytp-swatch-background-color",
+        style: "left: 0px; transform: scaleX(0);",
+      },
+      {
+        className: "ytp-progress-linear-live-buffer",
+        style: "left: 0px; transform: scaleX(0);",
+      },
+      {
+        className: "ytp-hover-progress ytp-hover-progress-light",
+        style: "left: 0px; transform: scaleX(0);",
+      },
+      { className: "ytp-ad-progress-list" },
+    ];
+    for (let i = 0; i < progressElementInfo.length; i++) {
+      const el = document.createElement("div");
+      el.className = progressElementInfo[i].className;
+      el.style.cssText = progressElementInfo[i].style;
+      ytpProgressBar.appendChild(el);
+    }
+    bar.appendChild(ytpProgressBar);
     bars.push(bar);
   });
   return bars;
@@ -82,6 +175,13 @@ async function makePlayerBar(playerBarInfo) {
 async function insertPlayerBar(playerBarInfo) {
   const playerBars = makePlayerBar(playerBarInfo);
   const container = document.querySelector(".ytp-chapters-container");
+
+  // !! 유튜브 재생바 제거
+  // while (container.childNodes[0]) {
+  //   container.removeChild(container.childNodes[0]);
+  // }
+  // !!
+
   (await playerBars).reverse().forEach((playerBar) => {
     container.insertBefore(playerBar, container.childNodes[0]);
   });
@@ -108,11 +208,15 @@ function makeSummaryContainer() {
   return ytSummaryContainer;
 }
 
-async function getSections() {
+async function getSections(data) {
   const sections = [];
-  for (let i = 0; i < 10; i++) {
+  chrome.runtime.sendMessage(data);
+  for (let i = 0; i < data.length; i++) {
     const section = document.createElement("section");
-    section.innerText = `section ${i}\n\n\n\n\n\n\n`;
+    const timeData = data[i].time.split(":").map((t) => parseInt(t));
+    const currentTime = timeData[0] * 60 + timeData[1];
+    // section.innerHTML = `${data[i].time} | ${data[i].text}`;
+    section.innerHTML = `<a class="yt-core-attributed-string__link yt-core-attributed-string__link--display-type yt-core-attributed-string__link--call-to-action-color" tabindex="0" href="/watch?v=aD73tXB0vYA&amp;t=${currentTime}s" rel="nofollow" target="" force-new-state="true">${data[i].time}</a> | ${data[i].text}`;
     sections.push(section);
   }
   return sections;
@@ -157,13 +261,13 @@ async function getTranscript() {
   //   })
   // ).json();
   // chrome.runtime.sendMessage({ apiResult });
-  fetch(
-    "https://waterboom.iptime.org:1032/?videoId=" +
-      window.location.search.split("?v=")[1]
-  )
-    .then((response) => response.json())
-    .then((json) => chrome.runtime.sendMessage(json))
-    .catch((e) => chrome.runtime.sendMessage({ err: e }));
+  // fetch(
+  //   "https://waterboom.iptime.org:1032/?videoId=" +
+  //     window.location.search.split("?v=")[1]
+  // )
+  //   .then((response) => response.json())
+  //   .then((json) => chrome.runtime.sendMessage(json))
+  //   .catch((e) => chrome.runtime.sendMessage({ err: e }));
 
   return transcriptWithTimeStamp;
 }
