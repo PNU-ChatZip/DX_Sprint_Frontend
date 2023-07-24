728087dcdb97d047f5b341903667dfe3fedd0656
daehee <gkseogml4959@gmail.com>
2023-07-24 13:18:09 +0900

    fix. 권한 수정

--- public/background.js
+++ public/background.js
@@ -1,4 +1,48 @@
 // console.log("[Youtube Summary Extension] connected...");
+let clientId =
+  "766002842369-26jdfm69klqq8s3sv7gqkug1if20a7ei.apps.googleusercontent.com";
+let redirectUri = `https://${chrome.runtime.id}.chromiumapp.org/`;
+let nonce = Math.random().toString(36).substring(2, 15);
+
+const googleLogin = async (sendResponse) => {
+  const authUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+
+  authUrl.searchParams.set("client_id", clientId);
+  authUrl.searchParams.set("response_type", "id_token");
+  authUrl.searchParams.set("redirect_uri", redirectUri);
+  // Add the OpenID scope. Scopes allow you to access the user’s information.
+  authUrl.searchParams.set("scope", "openid profile email");
+  authUrl.searchParams.set("nonce", nonce);
+  // Show the consent screen after login.
+  authUrl.searchParams.set("prompt", "consent");
+
+  chrome.identity.launchWebAuthFlow(
+    {
+      url: authUrl.href,
+      interactive: true,
+    },
+    (redirectUrl) => {
+      if (redirectUrl) {
+        // The ID token is in the URL hash
+        const urlHash = redirectUrl.split("#")[1];
+        const params = new URLSearchParams(urlHash);
+        const jwt = params.get("id_token");
+
+        // Parse the JSON Web Token
+        const base64Url = jwt.split(".")[1];
+        const base64 = base64Url.replace("-", "+").replace("_", "/");
+        const token = JSON.parse(atob(base64));
+
+        sendResponse({
+          data: { login: true, email: token.email, picture: token.picture },
+        });
+        console.log("token", token);
+      }
+      sendResponse({ data: { login: false } });
+    }
+  );
+  return true;
+};
 
 chrome.runtime.onInstalled.addListener(() => {
   // console.log("extension installed");
@@ -19,23 +63,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 
   if (message.login !== undefined) {
-    fetch("http://49.50.160.55:3000/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: message.login.email,
-        password: message.login.password,
-        username: message.login.username,
-      }),
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        if (json.login) sendResponse({ login: true });
-        else sendResponse({ login: false });
-      })
-      .catch((err) => console.log(err));
+    googleLogin(sendResponse);
     return true;
   }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,37 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-
-export default function App() {
-  return (
-    <>
-      <div>YouTube Comprehension</div>
-    </>
-  );
-}
 
 // export default function App() {
 //   const [isLogin, setIsLogin] = useState(false);
 //   const [userData, setUserData] = useState({});
-//   const [email, setEmail] = useState();
-//   const [password, setPassword] = useState();
-//   const [username, setUsername] = useState();
-
-//   useEffect(() => {
-//     chrome.runtime.sendMessage(
-//       {
-//         storage: "username",
-//       },
-//       function (res) {
-//         chrome.runtime.sendMessage(res);
-//         if (res.username) {
-//           chrome.runtime.sendMessage({ msg: "useEffect" });
-//           setUserData({ username: res.username });
-//           setIsLogin(true);
-//         } else {
-//           chrome.runtime.sendMessage(res);
-//         }
-//       }
-//     );
-//   }, []);
-
 //   return (
-//     <div className="App">
+//     <div class="App">
+//       <div>YouTube Comprehension</div>
 //       {isLogin ? (
-//         <div>{userData.username}</div>
-//       ) : (
 //         <>
-//           <form
-//             onSubmit={async (e) => {
-//               e.preventDefault();
-//               chrome.runtime.sendMessage({ msg: "fetch start" });
-//               chrome.runtime.sendMessage(
-//                 {
-//                   login: {
-//                     email,
-//                     password,
-//                     username,
-//                   },
-//                 },
-//                 (res) => {
-//                   if (res.login) {
-//                     setUserData({ email, username });
-//                     setIsLogin(true);
-//                   }
-//                 }
-//               );
-//               chrome.runtime.sendMessage({ login: true });
-//             }}>
-//             <div>
-//               <label>Email</label>
-//               <input
-//                 onChange={(e) => {
-//                   setEmail(e.target.value);
-//                 }}
-//               />
-//             </div>
-//             <div>
-//               <label>password</label>
-//               <input
-//                 onChange={(e) => {
-//                   setPassword(e.target.value);
-//                 }}
-//               />
-//             </div>
-//             <div>
-//               <label>username</label>
-//               <input
-//                 onChange={(e) => {
-//                   setUsername(e.target.value);
-//                 }}
-//               />
-//             </div>
-//             <input type="submit" value="로그인" />
-//           </form>
-//           {email} {password} {username}
+//           <img src={userData.picture} /> | {userData.email}
 //         </>
+//       ) : (
+//         <button
+//           onClick={() => {
+//             chrome.runtime.sendMessage({ login: true }, (res) => {
+//               if (res.data.login) {
+//                 setIsLogin(true);
+//                 setUserData(res.data);
+//               }
+//             });
+//           }}>
+//           구글 로그인
+//         </button>
 //       )}
 //     </div>
 //   );
 // }
+
+export default function App() {
+  return (
+    <div class="App">
+      <div>YouTube Comprehension</div>
+    </div>
+  );
+}
